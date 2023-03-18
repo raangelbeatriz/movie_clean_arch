37448fdd4f0d1a0c2de02a35a91024d6aac52628
Beatriz Rangel <raangelbeatriz@gmail.com>
2023-03-18 12:15:55 -0300

    add data layer with dto and repositories

---
+++ lib/feature/movie/data/repositories/get_similar_movies_repository_impl.dart
@@ -0,0 +1,30 @@
+import 'dart:developer';
+
+import 'package:movie_clean_arch/feature/movie/domain/entities/movie_details_entity.dart';
+import 'package:movie_clean_arch/feature/movie/domain/repositories/get_similiar_movies_repository.dart';
+
+import '../../../../core/data/services/dio_http_service.dart';
+import '../../../../core/exceptions/movie_exception.dart';
+import '../../../../core/utils/keys.dart';
+import '../dto/movie_dto.dart';
+
+class GetSimilarMoviesImpl implements GetSimiliarMoviesRepository {
+  DioHttpImpl _dioHttpImpl;
+  GetSimilarMoviesImpl(this._dioHttpImpl);
+  @override
+  Future<List<MovieDetailsEntity>> call(String id) async {
+    try {
+      List<MovieDetailsEntity> list = [];
+      final result =
+          await _dioHttpImpl.get('/movie/$id?api_key=${Keys.apiKey}');
+      log('The result é $result');
+      for (var element in result.data) {
+        list.add(MovieDetailsDto.fromJson(element));
+      }
+      return list;
+    } on Exception catch (e, s) {
+      log('Error while getting movie', error: e, stackTrace: s);
+      throw MovieException(message: 'Error while getting movie');
+    }
+  }
+}
